69f3a3a9e3692733edd57a214fd82735f06ff345
Nick <nicholas-borcherding@uiowa.edu>
2021-09-01 14:25:09 -0500

    update

--- R/Trex.Data.R
+++
@@ -1,7 +0,0 @@
-#' Amino acid properties and ranges for
-#' the autoencoder models along tensor
-#' @name Trex.Data
-#' @docType data
-#'
-#'
-NULL
--- R/trex_example.R
+++
@@ -1,7 +0,0 @@
-#' A seurat object of 3200 single T cells derived
-#' a CD3+ sorted severe COVID-19 patient
-#' @name trex_example
-#' @docType data
-#'
-#'
-NULL
